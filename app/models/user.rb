31c50a355cd5827ae29cc0d60fa5e4692fb94f63
Rahul Yadav <rahul.yadav@protonshub.com>
2020-02-06 01:05:52 +0530

    Changed the theme association

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
   has_many :following, through: :following_relationship, source: :following
 
 
-  has_many :themes
+  has_many :topics, class_name: 'Theme'
   has_many :answers
   has_many :questions
   has_many :follow_themes
